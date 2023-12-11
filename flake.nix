dd59a4de0a950788ab2a1225e4506fd6e5a22b24
Alison Jenkins <alison.juliet.jenkins@gmail.com>
2023-12-11 23:12:00 +0000

    Use the now working Conform format function! \o/

--- flake.nix
+++ flake.nix
@@ -248,6 +248,27 @@
               sh = [ "shfmt" ];
               terraform = [ "terraform_fmt" ];
             };
+
+            formatOnSave = ''
+              function(bufnr)
+                local ignore_filetypes = { "helm" }
+                if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                  return
+                end
+
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                  return
+                end
+
+                -- Disable autoformat for files in a certain path
+                local bufname = vim.api.nvim_buf_get_name(bufnr)
+                if bufname:match("/node_modules/") then
+                  return
+                end
+                return { timeout_ms = 500, lsp_fallback = true }
+              end
+            '';
           };
 
           copilot-cmp = {
