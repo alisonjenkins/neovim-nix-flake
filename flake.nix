84949bc16dda987f87246cb9fc908ae4de463460
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-03-13 08:43:38 +0000

    Rework cmp config to work with the new version of Nixvim

--- flake.nix
+++ flake.nix
@@ -317,58 +317,60 @@
             # fromVscode = [];
           };
 
-          nvim-cmp = {
+          cmp = {
             enable = true;
             autoEnableSources = true;
 
-            mapping = {
-              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-              "<C-e>" = "cmp.mapping.abort()";
-              "<C-f>" = "cmp.mapping.scroll_docs(4)";
-              "<C-n>" =
-                "cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }";
-              "<C-u>" = "cmp.mapping.complete({})";
-              "<C-p>" =
-                "cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }";
-              "<C-y>" = ''
-                cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }, {"i", "c"})'';
-              "<C-space>" = ''
-                cmp.mapping {
-                  i = cmp.mapping.complete(),
-                  c = function(
-                    _ --[[fallback]]
-                  )
-                  if cmp.visible() then
-                      if not cmp.confirm { select = true } then
-                        return
+            settings = {
+              mapping = {
+                "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+                "<C-e>" = "cmp.mapping.abort()";
+                "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                "<C-n>" =
+                  "cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }";
+                "<C-u>" = "cmp.mapping.complete({})";
+                "<C-p>" =
+                  "cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }";
+                "<C-y>" = ''
+                  cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }, {"i", "c"})'';
+                "<C-space>" = ''
+                  cmp.mapping {
+                    i = cmp.mapping.complete(),
+                    c = function(
+                      _ --[[fallback]]
+                    )
+                    if cmp.visible() then
+                        if not cmp.confirm { select = true } then
+                          return
+                        end
+                      else
+                        cmp.complete()
                       end
-                    else
-                      cmp.complete()
-                    end
-                  end,
+                    end,
+                  }
+                '';
+                "<tab>" = "cmp.config.diable";
+              };
+
+              snippet = {
+                expand = ''
+                  function(args)
+                  require("luasnip").lsp_expand(args.body)
+                  end
+                '';
+              };
+
+              sources = [
+                { name = "copilot"; }
+                { name = "nvim_lsp"; }
+                {
+                  name = "luasnip";
+                  option = { show_autosnippets = true; };
                 }
-              '';
-              "<tab>" = "cmp.config.diable";
+                { name = "path"; }
+                { name = "buffer"; }
+              ];
             };
-
-            snippet = {
-              expand.__raw = ''
-                function(args)
-                require("luasnip").lsp_expand(args.body)
-                end
-              '';
-            };
-
-            sources = [
-              { name = "copilot"; }
-              { name = "nvim_lsp"; }
-              {
-                name = "luasnip";
-                option = { show_autosnippets = true; };
-              }
-              { name = "path"; }
-              { name = "buffer"; }
-            ];
           };
 
           navbuddy = {
