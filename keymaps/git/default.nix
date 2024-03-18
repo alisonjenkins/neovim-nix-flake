ac25d1de8f1d32b24843712d12680d35b8285cb0
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-03-18 08:11:35 +0000

    Fix Gitlinker mappings

--- keymaps/git/default.nix
+++ keymaps/git/default.nix
@@ -74,7 +74,7 @@
   {
     mode = "n";
     key = "<leader>gLL";
-    action = ''lua require"gitlinker".get_repo_url()<cr>'';
+    action = ''<cmd>lua require"gitlinker".get_repo_url()<cr>'';
     options = {
       desc = "Git Linker Copy Repo URL";
       silent = true;
@@ -83,8 +83,7 @@
   {
     mode = "n";
     key = "<leader>gLb";
-    action = ''
-      lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>'';
+    action = ''<cmd>lua require"gitlinker".get_repo_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>'';
     options = {
       desc = "Git Linker Open In Browser";
       silent = true;
