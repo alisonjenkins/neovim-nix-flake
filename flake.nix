81efcb686e1af0bf9d305d662648114a98d9a2c9
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-08-08 09:06:41 +0100

    Temporarily disable treesitter and plugins that depend on it
    There is a dependency of treesitter that is currently broken in the nix
    unstable repos. Disabling treesitter and everything that depends on it
    until this is fixed.

--- flake.nix
+++ flake.nix
@@ -276,9 +276,9 @@
         fugitive.enable = true;
         gitignore.enable = true;
         gitsigns.enable = true;
-        headlines.enable = true;
+        headlines.enable = false;
         helm.enable = true;
-        hmts.enable = true;
+        hmts.enable = false;
         indent-blankline.enable = true;
         lastplace.enable = true;
         leap.enable = true;
@@ -292,15 +292,15 @@
         nvim-ufo.enable = true;
         octo.enable = true;
         project-nvim.enable = true;
-        rainbow-delimiters.enable = true;
+        rainbow-delimiters.enable = false;
         schemastore.enable = true;
         sleuth.enable = true;
         specs.enable = false;
         tmux-navigator.enable = true;
-        treesitter-refactor.enable = true;
-        treesitter-textobjects.enable = true;
-        ts-autotag.enable = true;
-        ts-context-commentstring.enable = true;
+        treesitter-refactor.enable = false;
+        treesitter-textobjects.enable = false;
+        ts-autotag.enable = false;
+        ts-context-commentstring.enable = false;
         typst-vim.enable = true;
         vim-css-color.enable = true;
         virt-column.enable = true;
@@ -519,17 +519,17 @@
         };
 
         neotest = {
-          enable = true;
+          enable = false;
 
-          adapters = {
-            bash.enable = true;
-            go.enable = true;
-            java.enable = true;
-            plenary.enable = true;
-            python.enable = true;
-            rust.enable = true;
-            zig.enable = true;
-          };
+          # adapters = {
+          #   bash.enable = true;
+          #   go.enable = true;
+          #   java.enable = true;
+          #   plenary.enable = true;
+          #   python.enable = true;
+          #   rust.enable = true;
+          #   zig.enable = true;
+          # };
 
           settings = {
             default_strategy = "integrated";
@@ -596,10 +596,14 @@
         };
 
         treesitter = {
-          enable = true;
+          enable = false;
 
           settings = {
-            ensure_installed = "all";
+            auto_install = true;
+            # ensure_installed = "all";
+            # ignore_install = [
+            #   "sql"
+            # ];
             textobjects.enable = true;
 
             highlight = {
@@ -623,7 +627,7 @@
         };
 
         treesitter-context = {
-          enable = true;
+          enable = false;
           settings = {
             max_lines = 4;
           };
