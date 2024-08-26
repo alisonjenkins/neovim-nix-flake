e87bb727c7945b2ea17ac223333f6ac260e6c8c4
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-08-26 16:49:30 +0100

    Update conform settings to deal with refactor in nixvim

--- flake.nix
+++ flake.nix
@@ -377,46 +377,48 @@
         conform-nvim = {
           enable = true;
 
-          formatters = {
-            codespell = {
-              prepend_args = ["-L" "crate"];
+          settings = {
+            formatters = {
+              codespell = {
+                prepend_args = ["-L" "crate"];
+              };
             };
+
+            formatters_by_ft = {
+              "_" = ["trim_whitespace"];
+              "*" = ["codespell"];
+              go = ["goimports" "golines" "gofmt" "gofumpt"];
+              javascript = [["prettierd" "prettier"]];
+              json = ["jq"];
+              lua = ["stylua"];
+              nix = ["alejandra"];
+              python = ["isort" "black"];
+              rust = ["rustfmt"];
+              sh = ["shfmt"];
+              terraform = ["terraform_fmt"];
+            };
+
+            format_on_save = ''
+              function(bufnr)
+                local ignore_filetypes = { "helm" }
+                if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                  return
+                end
+
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                  return
+                end
+
+                -- Disable autoformat for files in a certain path
+                local bufname = vim.api.nvim_buf_get_name(bufnr)
+                if bufname:match("/node_modules/") then
+                  return
+                end
+                return { timeout_ms = 1000, lsp_fallback = true }
+              end
+            '';
           };
-
-          formattersByFt = {
-            "_" = ["trim_whitespace"];
-            "*" = ["codespell"];
-            go = ["goimports" "golines" "gofmt" "gofumpt"];
-            javascript = [["prettierd" "prettier"]];
-            json = ["jq"];
-            lua = ["stylua"];
-            nix = ["alejandra"];
-            python = ["isort" "black"];
-            rust = ["rustfmt"];
-            sh = ["shfmt"];
-            terraform = ["terraform_fmt"];
-          };
-
-          formatOnSave = ''
-            function(bufnr)
-              local ignore_filetypes = { "helm" }
-              if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-                return
-              end
-
-              -- Disable with a global or buffer-local variable
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              -- Disable autoformat for files in a certain path
-              local bufname = vim.api.nvim_buf_get_name(bufnr)
-              if bufname:match("/node_modules/") then
-                return
-              end
-              return { timeout_ms = 1000, lsp_fallback = true }
-            end
-          '';
         };
 
         copilot-cmp = {
