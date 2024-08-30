13f36f9113ad926c953ce8a5ae0014a11304c373
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-08-30 22:21:30 +0100

    Example python project improvements

---
+++ tests/python/app/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def add(a, b):
+    return a + b
+
+
+def main():
+    print(add(1, 1))
+
+
+if __name__ == "__main__":
+    main()
--- tests/python/flake.nix
+++ tests/python/flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {flake-parts, ...}@inputs: 
+  outputs = {flake-parts, ...} @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "aarch64-darwin"
@@ -19,7 +24,7 @@
         pkgs,
         system,
         ...
-      }: let 
+      }: let
         devShell = pkgs.mkShell {
           packages = [
             (
@@ -29,8 +34,23 @@
             )
           ];
         };
+
+        example-python-project = inputs.pyproject-nix.lib.project.loadPyproject {
+          # Read & unmarshal pyproject.toml relative to this project root.
+          # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
+          projectRoot = ./.;
+        };
+
+        python = pkgs.python3;
       in {
         devShells.default = devShell;
+        packages.default = let
+          attrs = example-python-project.renderers.buildPythonPackage {inherit python;};
+        in
+          python.pkgs.buildPythonPackage (attrs
+            // {
+              # env.CUSTOM_ENVVAR = "hello";
+            });
       };
-  };
+    };
 }
