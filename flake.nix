2f5f4c9f577940a5fd85df5c7e1d2b47e2e0b5b5
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-09-17 07:03:15 +0100

    Fix mini.surround mappings

--- flake.nix
+++ flake.nix
@@ -652,13 +652,15 @@
             };
 
             surround = {
-              add = "gsa";
-              delete = "gsd";
-              find = "gsf";
-              find_left = "gsF";
-              highlight = "gsh";
-              replace = "gsr";
-              update_n_lines = "gsn";
+              mappings = {
+                add = "gsa";
+                delete = "gsd";
+                find = "gsf";
+                find_left = "gsF";
+                highlight = "gsh";
+                replace = "gsr";
+                update_n_lines = "gsn";
+              };
             };
           };
         };
