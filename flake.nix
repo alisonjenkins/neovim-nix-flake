16f774e01c29064a235235f52f4d92c0ae054bbb
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-28 13:11:47 +0000

    Fix dap-go

--- flake.nix
+++ flake.nix
@@ -530,10 +530,15 @@
               enable = true;
 
               extensions = {
-                dap-go.enable = true;
                 dap-python.enable = true;
                 dap-ui.enable = true;
                 dap-virtual-text.enable = true;
+
+                dap-go = {
+                  enable = true;
+
+                  delve.path = "${pkgs.delve}/bin/dlv";
+                };
               };
             };
 
