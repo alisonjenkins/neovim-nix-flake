79d8b7b498b5eaf050ee9a748a752219e86e83e2
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-10-29 11:09:04 +0000

    Revert "Setup blink-cmp"
    This reverts commit 18335bb45b5089e1bd84a902ea94973b8e20d79d.

--- flake.nix
+++ flake.nix
@@ -413,32 +413,6 @@
               };
             };
 
-            blink-cmp = {
-              enable = true;
-
-              settings = {
-                accept = {
-                  auto_brackets = {
-                    enabled = true;
-                  };
-                };
-
-                documentation = {
-                  auto_show = true;
-                };
-
-                highlight = {
-                  use_nvim_cmp_as_default = true;
-                };
-
-                trigger = {
-                  signature_help = {
-                    enabled = true;
-                  };
-                };
-              };
-            };
-
             # clipboard-image = {
             #   enable = true;
             #   clipboardPackage =
