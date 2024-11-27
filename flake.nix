38fb8281c73a65e9b32694d1aa16d1dc939e3f6b
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-11-27 07:16:29 +0000

    Add hardtime

--- flake.nix
+++ flake.nix
@@ -344,6 +344,7 @@
             fugitive.enable = true;
             gitignore.enable = true;
             gitsigns.enable = true;
+            hardtime.enable = true;
             helm.enable = true;
             helpview.enable = true;
             hmts.enable = true;
