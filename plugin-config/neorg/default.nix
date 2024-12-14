050178dc773871dd0e57a2fb3e6b3f45ad447d00
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-14 08:30:29 +0000

    Handle neorg modules rename to settings.load

--- plugin-config/neorg/default.nix
+++ plugin-config/neorg/default.nix
@@ -2,15 +2,17 @@
   neorg = {
     enable = true;
 
-    modules = {
-      "core.defaults" = {
-        __empty = null;
-      };
-      "core.dirman" = {
-        config = {
-          workspaces = {
-            home = "~/git/todo/home";
-            work = "~/git/todo/work";
+    settings = {
+      load = {
+        "core.defaults" = {
+          __empty = null;
+        };
+        "core.dirman" = {
+          config = {
+            workspaces = {
+              home = "~/git/todo/home";
+              work = "~/git/todo/work";
+            };
           };
         };
       };
