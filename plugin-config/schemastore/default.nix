67bf5008451be8c73c81f257e0afc0fae6e03313
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-14 08:46:37 +0000

    Add flux2 schemas to schemastore
    May have to rework fileMatch if it causes issues.

--- plugin-config/schemastore/default.nix
+++ plugin-config/schemastore/default.nix
@@ -4,6 +4,17 @@
 
     json = {
       enable = true;
+
+      settings = {
+        extra = [
+          {
+            description = "Flux2 schemas for when working with Flux Gitops resources.";
+            fileMatch = "*.yaml";
+            name = "flux2";
+            url = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/refs/heads/main/all.json";
+          }
+        ];
+      };
     };
 
     yaml = {
