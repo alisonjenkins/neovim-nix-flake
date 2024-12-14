829674654c8c51df771a2479e63deedd478817fa
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-14 10:06:27 +0000

    Add lazy loading for Octo

--- plugin-config/octo/default.nix
+++ plugin-config/octo/default.nix
@@ -3,5 +3,11 @@
     enable = true;
 
     settings = { suppress_missing_scope = { projects_v2 = true; }; };
+
+    lazyLoad = {
+      settings = {
+        cmd = "Octo";
+      };
+    };
   };
 }
