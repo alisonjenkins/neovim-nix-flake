2b7e189c0ffd038bb6fb9923d93da30e00478b48
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2024-12-24 10:14:11 +0000

    Fix dap-lldb codelldb path

--- flake.nix
+++ flake.nix
@@ -380,7 +380,7 @@
               // (import ./plugin-config/conform-nvim)
               // (import ./plugin-config/crates)
               // (import ./plugin-config/dap { inherit pkgs; })
-              // (import ./plugin-config/dap-lldb)
+              // (import ./plugin-config/dap-lldb { inherit pkgs; })
               // (import ./plugin-config/firenvim)
               // (import ./plugin-config/git-linker)
               // (import ./plugin-config/harpoon)
--- plugin-config/dap-lldb/default.nix
+++ plugin-config/dap-lldb/default.nix
@@ -1,3 +1,9 @@
-{
-  dap-lldb.enable = true;
+{ pkgs }: {
+  dap-lldb = {
+    enable = true;
+
+    settings = {
+      codelldb_path = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+    };
+  };
 }
