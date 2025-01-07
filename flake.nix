9d674e6d95364248b49eeb9609d075f528dc431e
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-01-07 18:25:09 +0000

    Move jump mappings to a jumps mapping file

--- flake.nix
+++ flake.nix
@@ -194,42 +194,6 @@
             [
               {
                 mode = "n";
-                key = "]d";
-                action = "<cmd>Lspsaga diagnostic_jump_next<cr>";
-                options = {
-                  desc = "Jump to next diagnostic";
-                  silent = true;
-                };
-              }
-              {
-                mode = "n";
-                key = "[d";
-                action = "<cmd>Lspsaga diagnostic_jump_prev<cr>";
-                options = {
-                  desc = "Jump to previous diagnostic";
-                  silent = true;
-                };
-              }
-              {
-                mode = "n";
-                key = "[r";
-                action = "<cmd>BaconPrevious<cr>";
-                options = {
-                  desc = "Bacon Previous Issue";
-                  silent = true;
-                };
-              }
-              {
-                mode = "n";
-                key = "]r";
-                action = "<cmd>BaconNext<cr>";
-                options = {
-                  desc = "Bacon Next Issue";
-                  silent = true;
-                };
-              }
-              {
-                mode = "n";
                 key = "gd";
                 action = "<cmd>Lspsaga goto_definition<cr>";
                 options = {
@@ -269,6 +233,7 @@
             ++ import ./keymaps/debugging
             ++ import ./keymaps/git
             ++ import ./keymaps/harpoon
+            ++ import ./keymaps/jumps
             ++ import ./keymaps/lsp
             ++ import ./keymaps/rust-lsp
             ++ import ./keymaps/search
---
+++ keymaps/jumps/default.nix
@@ -0,0 +1,38 @@
+[
+  {
+    mode = "n";
+    key = "[d";
+    action = "<cmd>Lspsaga diagnostic_jump_prev<cr>";
+    options = {
+      desc = "Jump to previous diagnostic";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "]d";
+    action = "<cmd>Lspsaga diagnostic_jump_next<cr>";
+    options = {
+      desc = "Jump to next diagnostic";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "[r";
+    action = "<cmd>BaconPrevious<cr>";
+    options = {
+      desc = "Bacon Previous Issue";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "]r";
+    action = "<cmd>BaconNext<cr>";
+    options = {
+      desc = "Bacon Next Issue";
+      silent = true;
+    };
+  }
+]
