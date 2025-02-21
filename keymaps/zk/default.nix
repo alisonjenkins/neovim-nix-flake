45610c2e8d58f85923dfcf44842f11017435468a
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-02-21 23:40:57 +0000

    Fix zk keymaps to ensure that I can access notes from anywhere

--- keymaps/zk/default.nix
+++ keymaps/zk/default.nix
@@ -2,7 +2,7 @@
   {
     mode = "n";
     key = "<leader>zc";
-    action = "<cmd>ZkCd<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkCd<cr>";
     options = {
       desc = "cd into the notebook root";
       silent = true;
@@ -11,7 +11,7 @@
   {
     mode = "n";
     key = "<leader>zn";
-    action = "<cmd>ZkNotes<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkNotes<cr>";
     options = {
       desc = "Open the Notes Picker";
       silent = true;
@@ -20,7 +20,16 @@
   {
     mode = "n";
     key = "<leader>zN";
-    action = "<cmd>ZkNew<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkNew<cr>";
+    options = {
+      desc = "Create and edit a new note";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>zd";
+    action = "<cmd>cd ~/git/zettelkasten | ZkNew { group = \"daily\" }<cr>";
     options = {
       desc = "Create and edit a new note";
       silent = true;
@@ -29,7 +38,7 @@
   {
     mode = "n";
     key = "<leader>zb";
-    action = "<cmd>ZkBacklinks<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkBacklinks<cr>";
     options = {
       desc = "Opens a notes picker for the backlinks of the current buffer";
       silent = true;
@@ -38,7 +47,7 @@
   {
     mode = "n";
     key = "<leader>zl";
-    action = "<cmd>ZkLinks<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkLinks<cr>";
     options = {
       desc = "Opens a notes picker for the outbound links of the current buffer";
       silent = true;
@@ -47,7 +56,7 @@
   {
     mode = "n";
     key = "<leader>zi";
-    action = "<cmd>ZkInsertLink<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkInsertLink<cr>";
     options = {
       desc = "Inserts a link at the cursor location or around the selected text.";
       silent = true;
@@ -56,7 +65,7 @@
   {
     mode = "n";
     key = "<leader>zt";
-    action = "<cmd>ZkTags<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkTags<cr>";
     options = {
       desc = "Opens a notes picker, filters for notes with the selected tags";
       silent = true;
@@ -66,7 +75,7 @@
     mode = "n";
     key = "<leader>zo";
     action =
-      "<cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<cr>";
+      "<cmd>cd ~/git/zettelkasten | ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<cr>";
     options = {
       desc = "Find notes by modification date";
       silent = true;
@@ -75,9 +84,18 @@
   {
     mode = "n";
     key = "<leader>zI";
-    action = "<cmd>ZkIndex<cr>";
+    action = "<cmd>cd ~/git/zettelkasten | ZkIndex<cr>";
     options = {
       desc = "Indexes the notebook";
+      silent = true;
+    };
+  }
+  {
+    mode = "n";
+    key = "<leader>zw";
+    action = "<cmd>cd ~/git/zettelkasten | ZkNew { group = \"weekly\" }<cr>";
+    options = {
+      desc = "Create and edit a new note";
       silent = true;
     };
   }
