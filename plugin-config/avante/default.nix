693e02b6f65f60b527172d262fd96ecf8f41344b
Alison Jenkins <alison.jenkins@brambles.com>
2025-05-22 10:38:04 +0100

    Setup Avante with Gemini

--- plugin-config/avante/default.nix
+++ plugin-config/avante/default.nix
@@ -6,6 +6,11 @@
 
     settings = {
       provider = "gemini";
+      gemini = {
+        api_key_name = [
+          "op" "item" "get" "Gemini API Key" "--fields" "label=password" "--reveal"
+        ];
+      };
     };
   };
 }
