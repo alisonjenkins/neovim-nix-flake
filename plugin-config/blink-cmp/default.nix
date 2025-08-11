8e649321b07396a945673af622692fcc7cfbfa01
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-08-11 07:23:23 +0100

    Fix blink ripgrep config deprecations

--- plugin-config/blink-cmp/default.nix
+++ plugin-config/blink-cmp/default.nix
@@ -183,16 +183,23 @@
             score_offset = 1;
             opts = {
               prefix_min_len = 3;
-              context_size = 5;
-              max_filesize = "1M";
               project_root_marker = ".git";
-              project_root_fallback = true;
-              search_casing = "--ignore-case";
               additional_rg_options = { };
               fallback_to_regex_highlighting = true;
               ignore_paths = { };
               additional_paths = { };
               debug = false;
+
+              backend = {
+                use = "ripgrep";
+
+                ripgrep = {
+                  context-size = 5;
+                  max_filesize = "1M";
+                  project_root_fallback = true;
+                  search_casing = "--ignore-case";
+                };
+              };
             };
           };
         };
