a48e38032207399562672d4c66926e3d04bade97
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-09-04 07:46:07 +0100

    Fix deprecated options in CodeCompanion config

--- plugin-config/codecompanion/default.nix
+++ plugin-config/codecompanion/default.nix
@@ -4,45 +4,45 @@
 
     settings = {
       adapters = {
-        gemini = {
-          __raw = ''
-            function()
-              return require('codecompanion.adapters').extend('gemini', {
-                  env = {
-                    api_key = "cmd:op read 'op://personal/Gemini API Key/password' --no-newline",
-                  },
-              })
-            end
-          '';
+        http = {
+          gemini = {
+            __raw = ''
+              function()
+                return require('codecompanion.adapters').extend('gemini', {
+                    env = {
+                      api_key = "cmd:op read 'op://personal/Gemini API Key/password' --no-newline",
+                    },
+                })
+              end
+            '';
+          };
+          # ollama = {
+          #   __raw = ''
+          #     function()
+          #       return require('codecompanion.adapters').extend('ollama', {
+          #           env = {
+          #               url = "http://127.0.0.1:11434",
+          #           },
+          #           schema = {
+          #               model = {
+          #                   default = 'qwen2.5-coder:latest',
+          #                   -- default = "llama3.1:8b-instruct-q8_0",
+          #               },
+          #               num_ctx = {
+          #                   default = 32768,
+          #               },
+          #           },
+          #       })
+          #     end
+          #   '';
+          # };
+          opts = {
+            log_level = "TRACE";
+            send_code = true;
+            use_default_actions = true;
+            use_default_prompts = true;
+          };
         };
-
-        # ollama = {
-        #   __raw = ''
-        #     function()
-        #       return require('codecompanion.adapters').extend('ollama', {
-        #           env = {
-        #               url = "http://127.0.0.1:11434",
-        #           },
-        #           schema = {
-        #               model = {
-        #                   default = 'qwen2.5-coder:latest',
-        #                   -- default = "llama3.1:8b-instruct-q8_0",
-        #               },
-        #               num_ctx = {
-        #                   default = 32768,
-        #               },
-        #           },
-        #       })
-        #     end
-        #   '';
-        # };
-      };
-
-      opts = {
-        log_level = "TRACE";
-        send_code = true;
-        use_default_actions = true;
-        use_default_prompts = true;
       };
 
       prompt_library = {
