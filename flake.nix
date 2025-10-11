eb49cad0e7147ad9b4ac9452bd4b6b89c6e72930
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-11 07:58:05 +0100

    fix: remvoe bad BufRead hook for Cargo.toml

--- flake.nix
+++ flake.nix
@@ -47,19 +47,6 @@
               pattern = [ "*.tf" " *.tfvars" " *.hcl" ];
               command = "set filetype=terraform";
             }
-            {
-              event = [ "BufRead" ];
-              pattern = [ "Cargo.toml" ];
-              command = ''
-                    vim.api.nvim_create_autocmd("BufRead", {
-                    group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-                    pattern = "Cargo.toml",
-                    callback = function()
-                        cmp.setup.buffer({ sources = { { name = "crates" } } })
-                    end,
-                })
-              '';
-            }
           ];
 
           extraConfigLua = ''
