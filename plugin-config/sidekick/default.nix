25421823545d10413b090a1ba4910a4b477c44e6
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-12 09:05:21 +0100

    fix: tidy up sidekick config formatting

--- plugin-config/sidekick/default.nix
+++ plugin-config/sidekick/default.nix
@@ -7,25 +7,30 @@
 
       prompts = {
         explain = "Explain this code";
+        optimize = "How can this code be optimized?";
+
         diagnostics = {
-          msg = "What do the diagnostics in this file mean?";
           diagnostics = true;
+          msg = "What do the diagnostics in this file mean?";
         };
+
         diagnostics_all = {
           msg = "Can you help me fix these issues?";
+
           diagnostics = {
             all = true;
           };
         };
+
         fix = {
           msg = "Can you fix the issues in this code?";
           diagnostics = true;
         };
+
         review = {
           msg = "Can you review this code for any issues or improvements?";
           diagnostics = true;
         };
-        optimize = "How can this code be optimized?";
       };
     };
   };
