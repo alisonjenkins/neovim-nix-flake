65fa7a16f80b8da3b748e57f4ebafedac982f311
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-13 08:48:15 +0100

    fix: re-enable combinePlugins
    It seems to work again!

--- flake.nix
+++ flake.nix
@@ -195,7 +195,7 @@
             };
 
             combinePlugins = {
-              enable = false;
+              enable = true;
 
               standalonePlugins = with pkgs.vimPlugins; [
                 conform-nvim
