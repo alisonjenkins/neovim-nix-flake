22b28d4c1306f325ab25ae201fd78feda4c52839
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-10-15 19:52:50 +0100

    feat: add obsidian frontmatter func

--- plugin-config/obsidian/default.nix
+++ plugin-config/obsidian/default.nix
@@ -11,6 +11,25 @@
         min_chars = 2;
       };
 
+      frontmatter.func.__raw = ''
+        function (note)
+          -- Add the title of the note as an alias.
+          if note.title then
+            note:add_alias(note.title)
+          end
+
+          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+          end
+
+          return out
+        end
+      '';
+
       workspaces = [
         {
           name = "startup";
