630493b7495c2f2c8d8443fcc410bc81752637cc
Alison Jenkins <1176328+alisonjenkins@users.noreply.github.com>
2025-11-08 07:37:53 +0000

    fix: remove trailing whitespace in dap-ui config

--- plugin-config/dap-ui/default.nix
+++ plugin-config/dap-ui/default.nix
@@ -5,7 +5,7 @@
     lazyLoad.settings = {
       cmd = [ "DapContinue" "DapToggleBreakpoint" "DapStepOver" "DapStepInto" "DapStepOut" ];
       keys = [
-        "<leader>db"  # Debug breakpoint  
+        "<leader>db"  # Debug breakpoint
         "<leader>dc"  # Debug continue
         "<leader>ds"  # Debug step
         "<leader>du"  # Debug UI
