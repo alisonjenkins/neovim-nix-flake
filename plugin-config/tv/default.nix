decacb9dd2a936a642e03f6692ce5cc7bbc3078e
Test User <test@test.com>
2025-12-11 21:57:53 +0000

    feat: configure TV to take the whole screen

--- plugin-config/tv/default.nix
+++ plugin-config/tv/default.nix
@@ -1,6 +1,14 @@
 {
   tv = {
     enable = true;
-    settings = {};
+    settings = {
+      window = {
+        width = 1;
+        height = 1;
+        border = "none";
+        title = " tv.nvim ";
+        title_pos = "center";
+      };
+    };
   };
 }
