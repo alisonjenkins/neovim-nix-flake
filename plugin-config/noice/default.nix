e71c635b9b03db9196baa4fc2f171a49b251e943
Alison Jenkins <alison.jenkins@civica.com>
2026-04-17 16:55:14 +0100

    fix(noice): disable LSP progress handler so fidget.nvim can display progress
    - noice's built-in progress handler overrides
      vim.lsp.handlers["$/progress"], which prevents the LspProgress autocmd
      from firing; fidget.nvim relies on that autocmd and was therefore
      silently invisible
    - Setting progress.enabled = false hands the $/progress notification back
      to the default path, allowing fidget to receive and display LSP progress
      normally

--- plugin-config/noice/default.nix
+++ plugin-config/noice/default.nix
@@ -3,9 +3,16 @@
     enable = true;
     package = pkgs.master.vimPlugins.noice-nvim;
 
-    settings.lsp.override = {
-      "vim.lsp.util.convert_input_to_markdown_lines" = true;
-      "vim.lsp.util.stylize_markdown" = true;
+    settings.lsp = {
+      # Let fidget.nvim handle LSP progress display; noice's progress handler
+      # overrides vim.lsp.handlers["$/progress"] and prevents LspProgress
+      # autocmd from firing, which makes fidget invisible.
+      progress.enabled = false;
+
+      override = {
+        "vim.lsp.util.convert_input_to_markdown_lines" = true;
+        "vim.lsp.util.stylize_markdown" = true;
+      };
     };
   };
 }
